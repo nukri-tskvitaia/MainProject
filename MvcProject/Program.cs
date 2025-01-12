9f20e9d02c9f9d2ffe8003970147b8966a4aeda1
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2025-01-12 21:55:09 +0400

    Add essential scaffolded identity pages and dapper.

--- MvcProject/Program.cs
+++ MvcProject/Program.cs
@@ -1,4 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcProject.Areas.Identity.Data;
+using MvcProject.Data;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services
+    .AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services
+    .AddTransient<IDbConnection>(sp => new SqlConnection(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
