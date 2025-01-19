09f2f5e42cf42b1600e2370ec18e4428001cee30
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2025-01-19 20:09:42 +0400

    Add current balance and username on nav bar.

--- MvcProject/Controllers/HomeController.cs
+++ MvcProject/Controllers/HomeController.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using MvcProject.Data.Repositories;
 using MvcProject.Models;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace MvcProject.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUserRepository _userRepository;
+    private readonly IWalletRepository _walletRepository;
 
-    public HomeController(ILogger<HomeController> logger, IUserRepository user, UserManager<User> userManager)
+    public HomeController(ILogger<HomeController> logger, IUserRepository user, IWalletRepository walletRepository)
     {
         _logger = logger;
+        _userRepository = user;
+        _walletRepository = walletRepository;
     }
 
     public IActionResult Index()
@@ -38,6 +43,48 @@ public class HomeController : Controller
     public IActionResult Logout()
     {
         return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsername()
+    {
+        if (!User?.Identity?.IsAuthenticated ?? true)
+        {
+            return Unauthorized(new { success = false, message = "User is not authenticated." });
+        }
+
+        var userId = User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null)
+        {
+            return Unauthorized(new { success = false, message = "User ID is missing or invalid." });
+        }
+
+        var username = await _userRepository.GetUsernameAsync(userId);
+
+        if (string.IsNullOrEmpty(username))
+        {
+            return NotFound(new { success = false, message = "Username not found." });
+        }
+
+        return Ok(new { success = true, name = username });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUserBalance()
+    {
+        if (!User?.Identity?.IsAuthenticated ?? true)
+        {
+            return Unauthorized(new { success = false, message = "User is not authenticated." });
+        }
+
+        var userId = User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null)
+        {
+            return Unauthorized(new { success = false, message = "User ID is missing or invalid." });
+        }
+
+        var balance = await _walletRepository.GetUserBalanceAsync(userId);
+        return Ok(new { success = true, balance });
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
